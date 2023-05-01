395e32c5556ed6621390dc449d406ea89d20692b
Ofir Gur <ofir.gur@browzwear.com>
2023-05-01 16:19:07 +0300

    Authentication

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -5,13 +5,17 @@ import { StylesProvider } from '@material-ui/core/styles';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
     return <div>
         <StylesProvider>
             <Router history={history}>
                 <Switch>  
-                    <Route path="/auth/signin" component={Signin} />  
-                    <Route path="/auth/signup" component={Signup} />  
+                    <Route path="/auth/signin">
+                        <Signin onSignIn={onSignIn} />
+                    </Route>  
+                    <Route path="/auth/signup">
+                        <Signup onSignIn={onSignIn} />
+                    </Route>  
                 </Switch>
             </Router>
         </StylesProvider>
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Progress from './components/Progress';
@@ -7,13 +7,16 @@ const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
     return (
     <BrowserRouter>
         <div>
-            <Header />
+            <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
             <Suspense fallback={<Progress />}>
                 <Switch>
-                    <Route path="/auth" component={AuthLazy} />
+                    <Route path="/auth">
+                        <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                    </Route>
                     <Route path="/" component={MarketingLazy} />
                 </Switch>
             </Suspense>
