20a74f4bda0f3053c14bd54cb70374f2841aa45a
Ofir Gur <ofir.gur@browzwear.com>
2023-05-01 14:17:22 +0300

    Auth added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 export default () => {
     return (
     <BrowserRouter>
         <div>
             <Header />
-            <MarketingApp />
+            <Suspense fallback={<Progress />}>
+                <Switch>
+                    <Route path="/auth" component={AuthLazy} />
+                    <Route path="/" component={MarketingLazy} />
+                </Switch>
+            </Suspense>
         </div>
+        
     </BrowserRouter>
     );
 };
